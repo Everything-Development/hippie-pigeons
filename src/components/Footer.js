698ef4105ccc2bff845fec5be03c1e45af08f2cd
mishagavura <1mgavura@gmail.com>
2022-06-23 11:00:05 +0300

    added evdev

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,11 @@ import opensea from '../assets/opensea.png'
 import twitter from '../assets/twitter.svg'
 import discord from '../assets/discord.svg'
 import instagram from '../assets/instagram.png'
-
+import logo_evdev from '../assets/logo_mail_small.png'
 
 const Footer = () => {
   return (
+<>
     <footer>
         <p className='footer-copyright'>Hippie Pigeons Copyright 2022</p>
         <img className='logo-footer' src={logo_small}/>
@@ -18,6 +19,13 @@ const Footer = () => {
             <img src={discord}/>
         </div>
     </footer>
+    <div>
+        <a href='https://www.figma.com/proto/2UCIavg8otRVwnSMfphiFx/EVDEV?node-id=8%3A366&scaling=scale-down-width&page-id=0%3A1' className='evdev-section'>
+          <p>developed by</p>
+          <a href='https://www.figma.com/proto/2UCIavg8otRVwnSMfphiFx/EVDEV?node-id=8%3A366&scaling=scale-down-width&page-id=0%3A1'><img src={logo_evdev}/></a>
+        </a>
+    </div>
+</>
   )
 }
 
